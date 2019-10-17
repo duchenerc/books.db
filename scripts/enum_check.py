ab034f2f962c6e45aa69013dc3d736939be23a79
Ryan Duchene <rcdvxd@umsystem.edu>
2019-10-17 12:05:21 -0500

    First pass at handling enums Genres still need work

---
+++ scripts/enum_check.py
@@ -0,0 +1,99 @@
+from csv import reader
+import re
+from isbn_check import INVENTORY
+
+CONDITION_COL = 3
+JACKET_COL = 4
+BINDING_COL = 5
+GENRE_COL = 6
+
+
+def parse_condition(condition_line):
+    condition = condition_line.strip().lower()
+
+    if len(condition) == 0:
+        return "very good"
+
+    elif condition == "poor, very poor":
+        return "very poor"
+    
+    elif condition == "fine":
+        return "fair"
+    
+    elif condition == "new":
+        return "as new"
+    
+    else:
+        return condition
+
+def parse_jacket(jacket_line):
+    jacket = jacket_line.strip().lower()
+
+    if len(jacket) == 0:
+        return "no jacket"
+    
+    elif jacket == "no":
+        return "no jacket"
+
+    else:
+        return jacket    
+
+def parse_binding(binding_line):
+    return binding_line.strip().lower()
+
+def main():
+    fin = open(INVENTORY, newline="")
+    csvin = reader(fin)
+
+    next(csvin) # skip headers
+
+    conditions_db = []
+    jackets_db = []
+    bindings_db = []
+    genres_db = []
+
+    for entry in csvin:
+        condition_line = entry[CONDITION_COL]
+        jacket_line = entry[JACKET_COL]
+        binding_line = entry[BINDING_COL]
+        genre = entry[GENRE_COL].strip().lower()
+
+        condition = parse_condition(condition_line)
+        jacket = parse_jacket(jacket_line)
+        binding = parse_binding(binding_line)
+        
+        # TODO: handle genres
+        if len(genre) == 0:
+            genre = "__unknown__"
+        
+
+        if condition not in conditions_db:
+            conditions_db.append(condition)
+
+        if jacket not in jackets_db:
+            jackets_db.append(jacket)
+
+        if binding not in bindings_db:
+            bindings_db.append(binding)
+
+        if genre not in genres_db:
+            genres_db.append(genre)
+
+
+    fin.close()
+    
+    for condition in conditions_db: print(condition)
+    print()
+
+    for jacket in jackets_db: print(jacket)
+    print()
+
+    for binding in bindings_db: print(binding)
+    print()
+
+    for genre in genres_db: print(genre)
+    print()
+
+
+if __name__ == "__main__":
+    main()
